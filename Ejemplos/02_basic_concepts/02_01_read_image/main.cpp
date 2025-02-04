04fa5c31afe29ed387149381640aa4ac7d26f234
Alberto Leon Luengo <a.leon.2020@alumnos.urjc.es>
2025-02-04 10:39:57 +0100

    UPLOADING FILES ...

--- Ejemplos/02_basic_concepts/02_01_read_image/main.cpp
+++ Ejemplos/02_basic_concepts/02_01_read_image/main.cpp
@@ -1,19 +1,17 @@
-/**
- * Read image - sample code
- * @author José Miguel Guerrero
- */
-
 #include <opencv2/highgui.hpp>
 
 int main()
 {
   // Create image variable
+  // Las imágenes en OpenCV son tipo cv::Mat
   cv::Mat image;
 
   // Read image
+  // Lectura de la imagen: Mat cv::imread(const string &filename, int flags=IMREAD_COLOR)
   image = cv::imread("../../data/lena.jpg", cv::IMREAD_COLOR);
 
   // Show image
+  // Mostrar la imagen
   cv::imshow("TEST IMAGE", image);
 
   // Wait to press a key
--- Ejemplos/02_basic_concepts/02_01_read_image/main_namespace.cpp
+++ Ejemplos/02_basic_concepts/02_01_read_image/main_namespace.cpp
@@ -1,21 +1,21 @@
-/**
- * Read image - sample code
- * @author José Miguel Guerrero
- */
-
 #include <opencv2/highgui.hpp>
 
+// Igual que en el ejemplo anterior, pero usando el namespace
+// evitamos tener que utilizar elementos de OpenCV con cv::
 using namespace cv;
 
 int main()
 {
   // Create image variable
+  // Las imágenes en OpenCV son tipo cv::Mat
   Mat image;
 
   // Read image
+  // Lectura de la imagen: Mat cv::imread(const string &filename, int flags=IMREAD_COLOR)
   image = imread("../../data/lena.jpg", IMREAD_COLOR);
 
   // Show image
+  // Mostrar la imagen
   imshow("TEST IMAGE", image);
 
   // Wait to press a key
