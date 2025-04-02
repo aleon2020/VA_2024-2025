e775ef0eb5c6d7066f02837bb8882e1ad4417994
Alberto Leon Luengo <a.leon.2020@alumnos.urjc.es>
2025-04-02 14:22:01 +0200

    UPLOADING FILES ...

---
+++ Prácticas/Práctica 3: Geometría, Calibración y 3D/src/Opción 3: stereo/main.cpp
@@ -0,0 +1,21 @@
+#include <opencv2/highgui.hpp>
+
+int main()
+{
+  // Create image variable
+  // Las imágenes en OpenCV son tipo cv::Mat
+  cv::Mat image;
+
+  // Read image
+  // Lectura de la imagen: Mat cv::imread(const string &filename, int flags=IMREAD_COLOR)
+  image = cv::imread("pinkballs.jpeg", cv::IMREAD_COLOR);
+
+  // Show image
+  // Mostrar la imagen
+  cv::imshow("OPTION 3", image);
+
+  // Wait to press a key
+  cv::waitKey(0);
+
+  return 0;
+}
